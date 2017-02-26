c87df2f5ac5b52b4dcaead56b49c72db4f767c06
Yungno <Yungno@Yungno-MacBook-Pro.local>
2017-02-26 13:42:24 +0800

    ~

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include "basic_headers.h"
 #include "geo_definitions.h"
 
-int main(){
-      formula a,b,c;
-      a.initFormula("y=-3x^2-2x-3");
-      b.initFormula(-3,2,1);
-      c.initFormula(3);
-        a.printFormula(); 
-      b.printFormula();
-      c.printFormula();
+int main() {
+  formula a, b, c;
+  a.initFormula("y=-3x^2-2x-3");
+  b.initFormula(-3, 2, 1);
+  c.initFormula(3);
+  a.printFormula();
+  b.printFormula();
+  c.printFormula();
 }
