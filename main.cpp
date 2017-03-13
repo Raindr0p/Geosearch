3164f009931481ef9ebafa34654bd7a3efd23bff
Yungno <Yungno@Yungno-MacBook-Pro.local>
2017-03-13 22:11:39 +0800

    Rewrite definitions#

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #define VNAME(name) (#name)
 #include "basic_headers.h"
-#include "geo_definitions.h"
+#include "definitions.h"
 
 int main() {
-  formula a, b, c;
-  a.initFormula("y=-3x^2-2x-3");
-  b.initFormula(-3, 2, 1);
-  c.initFormula(3);
-  a.printFormula();
-  b.printFormula();
-  c.printFormula();
+  point A, B, C;
+  A.initPoint(1, 2);
+  B.initPoint(0, 0);
+  C.initPoint(1, 3);
+  line a, b;
+
+  a.initLine(A, B);
+  b.initLine(C, B);
+
+  linearPair alp;
+  alp.initLP(a, b);
+  cout << alp;
 }
